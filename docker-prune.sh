9cc387abbf689107ff16ca9f60f8b0f033e3a133
mooreBrendan <moore.brendanm@comcast.net>
2025-04-23 01:47:49 -0400

    moved to .tech domain

--- docker-prune.sh
+++ docker-prune.sh
@@ -1,4 +1,4 @@
 apt-get update
 apt-get upgrade -y
 docker system prune -f
-curl -d "pruned docker" -k -L ntfy.bmoore.local/server-notifs
+curl -d "pruned docker" -k -L ntfy.bmoore.tech/server-notifs
